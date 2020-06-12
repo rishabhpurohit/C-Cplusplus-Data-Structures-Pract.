39091daaddf1be3d38036cf9110f5aaf09422d21
Rishabh Purohit <rishabh.purohit123456789@gmail.com>
2020-06-12 17:24:16 +0530

    re3

---
+++ Udacity C++/debug.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+// using namespace std;
+int main() {
+  std::vector<std::string> brothers {
+    "David", "Singhal",
+        "oops};
+        for (std::string const &brother : brothers) {
+      std::cout << "Hello" << brother << '\n';
+    }
+    return 0;
+  }
